2cba4955832a501b1ed5c0e4b7ce9e6db56b0d06
Lee-web-bit <hungtqph32805@fpt.edu.vn>
2023-11-15 22:20:20 +0700

    update

---
+++ admin/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <!-- Mirrored from wrraptheme.com/templates/hexabit/html/light/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 04 Nov 2023 15:54:23 GMT -->
+  <head>
+    <title>:: HexaBit :: Home</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+    />
+    <meta name="description" content="HexaBit Bootstrap 4x Admin Template" />
+    <meta name="author" content="WrapTheme, www.thememakker.com" />
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <!-- VENDOR CSS -->
+    <link
+      rel="stylesheet"
+      href="../view/assets/vendor/bootstrap/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../view/assets/vendor/font-awesome/css/font-awesome.min.css"
+    />
+
+    <link rel="stylesheet" href="../view/assets/vendor/charts-c3/plugin.css" />
+    <link
+      rel="stylesheet"
+      href="../view/assets/vendor/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../view/assets/vendor/chartist/css/chartist.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../view/assets/vendor/chartist-plugin-tooltip/chartist-plugin-tooltip.css"
+    />
+    <link rel="stylesheet" href="../view/assets/vendor/toastr/toastr.min.css" />
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="../view/light/assets/css/main.css" />
+    <link rel="stylesheet" href="../view/light/assets/css/color_skins.css" />
+  </head>
+  <body class="theme-orange">
+    <!-- Page Loader -->
+    
+  
