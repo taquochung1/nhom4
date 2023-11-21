2e2a5063b4ce3cb908bc9932eebd9e0ccaa3e6d7
Lee-web-bit <hungtqph32805@fpt.edu.vn>
2023-11-21 22:19:47 +0700

    update

--- admin/taikhoan/addtk.php
+++ admin/taikhoan/addtk.php
@@ -1,37 +1,46 @@
-<div id="wrapper">
+<body class="theme-orange">
+  <div id="wrapper">
     <div id="left-sidebar" class="sidebar">
-        <div class="navbar-brand">
-            <a href="index.html"><img src="https://wrraptheme.com/templates/hexabit/html/assets/images/icon-dark.svg" alt="HexaBit Logo" class="img-fluid logo" /><span>HexaBit</span></a>
-            <button type="button" class="btn-toggle-offcanvas btn btn-sm btn-default float-right">
-                <i class="lnr lnr-menu fa fa-chevron-circle-left"></i>
-            </button>
-        </div>
-        <div class="sidebar-scroll">
+      <div class="navbar-brand">
+        <a href="index.html"><img src="https://wrraptheme.com/templates/hexabit/html/assets/images/icon-dark.svg" alt="HexaBit Logo" class="img-fluid logo" /><span>HexaBit</span></a>
+        <button type="button" class="btn-toggle-offcanvas btn btn-sm btn-default float-right">
+          <i class="lnr lnr-menu fa fa-chevron-circle-left"></i>
+        </button>
+      </div>
+      <div class="sidebar-scroll">
 
-            <nav id="left-sidebar-nav" class="sidebar-nav">
-                <ul id="main-menu" class="metismenu">
-                    <li class="active">
-                        <a href="index.php"><i class="icon-home"></i><span>Trang chủ</span></a>
-                    </li>
-                    <li>
-                        <a href="index.php?act=adddm" class="has-arrow"><span>Danh mục</span></a>
-                    </li>
-                    <li>
-                        <a href="index.php?act=addsp" class="has-arrow"><span>Sản phẩm</span></a>
-                    </li>
-                    <li>
-                        <a href="index.php?act=addmakm" class="has-arrow"><span>Mã giảm giá</span></a>
-                    </li>
-                    <li>
-                        <a href="index.php?act=ctsp" class="has-arrow"><span>Chi tiết</span></a>
-                    </li>
-                    <li>
-                        <a href="index.php?act=addtk" class="has-arrow"><span>Tài Khoản </span></a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <nav id="left-sidebar-nav" class="sidebar-nav">
+          <ul id="main-menu" class="metismenu">
+            <li class="active">
+              <a href="index.php"><i class="icon-home"></i><span>Trang chủ</span></a>
+            </li>
+            <li>
+              <a href="index.php?act=adddm" class="has-arrow"><span>Danh mục</span></a>
+            </li>
+            <li>
+              <a href="index.php?act=addsp" class="has-arrow"><span>Sản phẩm</span></a>
+            </li>
+            <li>
+              <a href="index.php?act=addmakm" class="has-arrow"><span>Mã khuyến mại</span></a>
+            </li>
+            <li>
+              <a href="index.php?act=ctsp" class="has-arrow"><span>Chi tiết</span></a>
+            </li>
+            <li>
+              <a href="index.php?act=addtk" class="has-arrow"><span>Tài Khoản </span></a>
+            </li>
+            <li>
+              <a href="index.php?act=addbl" class="has-arrow"><span>Bình Luận </span></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
+
+    <div id="main-content">
+
+    </div>
+  </div>
 
     <div id="main-content">
 
